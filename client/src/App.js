6571bee97f9f3c6df3cbad6fd381a72db9f96da0
SungChul Hong <schong17@myseneca.ca>
2021-01-06 22:54:46 -0500

    Add Node.js Express Server Module

---
+++ client/src/App.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import Table from '@material-ui/core/Table'
+import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper'
+import TableHead from '@material-ui/core/TableHead'
+import TableBody from '@material-ui/core/TableBody'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import TableContainer from '@material-ui/core/TableContainer';
+import { withStyles} from '@material-ui/core/styles'
+
+const styles = theme => ({
+  root:{
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 400
+  }
+})
+
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : 'SungChul',
+  'birthday' : '123456',
+  'gender' : 'Male',
+  'job' : 'Web Developer'
+},{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : 'Matthew',
+  'birthday' : '456789',
+  'gender' : 'Male',
+  'job' : 'Dental Technician'
+
+},{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : 'Hong',
+  'birthday' : '123789',
+  'gender' : 'Male',
+  'job' : 'Student'
+
+}]
+
+class App extends React.Component {
+  render(){
+    const {classes} = this.props;
+    return (
+      <Paper className={classes.root} >
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Image</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Birth</TableCell>
+              <TableCell>Gender</TableCell>
+              <TableCell>Job</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => {
+              return (<Customer 
+                key={c.id}
+                id={c.id}
+                image= {c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />)
+            })}
+            </TableBody>
+        </Table>
+      </Paper>
+  
+       
+    );
+  }
+ 
+}
+
+export default withStyles(styles)(App);
